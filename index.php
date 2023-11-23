4ba8f083c80658f46e36e24299f335cec529bdd9
Limson91 <matteo.a.colautti@gmail.com>
2023-11-23 17:03:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$pw_char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%&/()=?^+*-_'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strong Password Generator</title>
+</head>
+
+<body>
+    <form action="" method="GET">
+        <input type="text" name="pw-length">
+        <input type="submit" valure="Send">
+    </form>
+
+    <h3>Your secure password is:</h3>
+</body>
+</html>
